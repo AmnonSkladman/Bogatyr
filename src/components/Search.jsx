245789c2876ad3260475b3b451546abe212f7873
Amnon Skladman <askladm@Amnons-MacBook-Pro.local>
2019-04-25 16:10:18 -0400

    Cleanup

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
 
 const Search = ({ search }) => {
-  // [state, setState] React Hook
   const [query, setQuery] = useState('');
 
-  // populates the query
   const handleChangeQuery = e => {
     setQuery(e.target.value);
   };
 
-  // resets the input
   const resetInputField = () => {
     setQuery('');
   };
 
-  // searches using the value and resets the field
   const searchBooks = e => {
     e.preventDefault();
     search(query);
@@ -23,11 +19,7 @@ const Search = ({ search }) => {
 
   return (
     <form className="search">
-      <input
-        value={query}
-        onChange={handleChangeQuery}
-        type="text"
-      />
+      <input value={query} onChange={handleChangeQuery} type="text" />
       <input onClick={searchBooks} type="submit" value="SEARCH" />
     </form>
   );
