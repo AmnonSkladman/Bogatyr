f2759190cefe8c363f3adac949184ccdd493d7d4
Amnon Skladman <askladm@Amnons-MacBook-Pro.local>
2019-04-26 13:34:36 -0400

    Added Normalize CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './App.css';
+import './styles/Normalize.css';
+import './styles/App.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
