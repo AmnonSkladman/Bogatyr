36caf429920cacbab82f0f8de9976249a5fc5158
Amnon Skladman <amnon.skladman@gmail.com>
2019-04-26 21:30:08 -0400

    Updates

---
+++ precache-manifest.b9c6077b19f2e68f099b260618e9e559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ae3622ae2ea5b3eab14183dbb98a45",
+    "url": "/Bogatyr/index.html"
+  },
+  {
+    "revision": "5d01e0f299741d98afc5",
+    "url": "/Bogatyr/static/css/main.ddd3b99b.chunk.css"
+  },
+  {
+    "revision": "ed73174ffbf4f9b0fdb2",
+    "url": "/Bogatyr/static/js/2.2bff1922.chunk.js"
+  },
+  {
+    "revision": "5d01e0f299741d98afc5",
+    "url": "/Bogatyr/static/js/main.347fe92f.chunk.js"
+  },
+  {
+    "revision": "e8c8b69a3909db219dfd",
+    "url": "/Bogatyr/static/js/runtime~main.7e8295d3.js"
+  }
+]);
--- src/utils/api.js
+++
@@ -1,1 +0,0 @@
-export const BOOK_API_URL = "https://www.googleapis.com/books/v1/volumes?q=''";
